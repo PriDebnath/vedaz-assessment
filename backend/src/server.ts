31bf1f6f5b56f6e66cc26622aae093fcaa7a3d63
Pritam <debnathpritam0802@gmail.com>
2026-07-01 19:01:07 +0530

    setup postgresdb

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,11 @@
 import {  env} from "./utils/load-env"; // should be  at top
 import  app  from "./app"
 import { connectDB } from "./database/connection";
-import { connectRedis } from "./utils/redis/connect-redis";
 
 async function startServer() {
   const { PORT } = env;
 
   await connectDB()
-  await connectRedis()
   
   app.listen(PORT || 8000, () => {
     console.log(`🟩 "Express with PG" Server running on port ${PORT}`);
